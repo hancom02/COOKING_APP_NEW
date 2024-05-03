450d9943d809ceb96d11d302612af3a4d0fe6c2e
ngochan <20520484@gm.uit.edu.vn>
2024-05-04 00:57:32 +0700

    create CookbookComponent

---
+++ src/components/CookBookComponent.js
@@ -0,0 +1,124 @@
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
+
+//ICON
+//import from 'ionicons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+function CookBookComponent(props) {
+    const{
+        name,
+        introduce,
+        image,
+        likes,
+        recipe_amount
+    } = props;
+
+    return(
+        <View style={styles.container}>
+            <View style={styles.imageBackgroundView}> 
+                <ImageBackground
+                    source={{uri: image}}
+                    style={styles.imageBackground}
+                >
+                    <View style={styles.contentContainer}>
+                        <View style={styles.iconContainer}>
+                            <Ionicons name="happy" size={24} color={'orange'} />
+                        </View>
+                        <Text style={styles.textNameCookBook}>{name}</Text>
+                        <Text style={styles.textIntroduce}>{introduce}</Text>
+                        <View style={styles.like_recipe_container}>
+                            <Text style={styles.textLikeRecipes}>{likes}  Likes</Text>
+                            <Text style={styles.divider}>|</Text>
+                            <Text style={styles.textLikeRecipes}>{recipe_amount}  Recipes</Text>
+                        </View>
+                    </View>
+                </ImageBackground>
+            </View>
+
+            {/* <View style={styles.contentContainer}>
+                    <Text>Traditional Vietnamese cousine</Text>
+            </View> */}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 500,
+        backgroundColor: 'white',
+        borderRadius: 16,
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center'
+        //backgroundColor: 'green'
+    },
+    imageBackgroundView: {
+        width: '94%',
+        height: '95%',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        alignItems: 'center',
+        overflow: 'hidden',
+        //backgroundColor: 'orange',
+
+    },
+    imageBackground: {
+        width: '100%',
+        height: '94%',
+        justifyContent: 'flex-end',
+        //backgroundColor: 'green'
+    },
+    contentContainer: {
+        backgroundColor: 'white',
+        width: '90%',
+        height: '60%',
+        alignSelf: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 16,
+        paddingHorizontal: 38,
+        paddingVertical: 16
+    },
+    iconContainer: {
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+        borderColor: 'grey',
+        borderWidth: 0.5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20
+    },
+    textNameCookBook: {
+        fontSize: 24,
+        fontWeight: '700',
+        color: 'black',
+        textAlign: 'center',
+        marginBottom: 10,
+    },
+    textIntroduce: {
+        textAlign: 'center'
+    },
+    like_recipe_container: {
+        flex: 1,
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        //backgroundColor: 'green'
+    },
+    textLikeRecipes: {
+        flex: 1,
+        fontWeight: '700',
+        textAlign: 'center'
+
+    },
+    divider: {
+        marginHorizontal: 0, 
+        color: 'gray', 
+    }
+
+    
+})
+
+export default CookBookComponent;
--- src/screens/Home/views/HomeMainView.js
+++ src/screens/Home/views/HomeMainView.js
@@ -1,17 +1,68 @@
-import { Image, StyleSheet, Text, View } from "react-native"
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native"
 import { create } from "react-test-renderer";
+import CookBookComponent from "../../../components/CookBookComponent";
+import Swiper from "react-native-swiper";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 
 const HomeMainView = (props) => {
     const {user, image} = props;
 
+    const propsComponent = {
+        name: "Vietnames Traditional Cuisine",
+        introduce: "Keep it easy with these simple but delicious recipes.",
+        image: "https://media.vneconomy.vn/w800/images/upload/2024/01/13/phos-1920x1280.jpg",
+        likes: 17,
+        recipe_amount: 8
+    };
+
     //console.log(image[0]);
+
+    const handleSwiperIndexChange = (index) => {
+        console.log("Chuyển hướng tới detail cookbox index: ", index);
+    }
 
     return (
         <View style={styles.container}>
-            <Text> Home Main View</Text>
-            <Text>{user.name}</Text>
+            <View style={styles.userContainer}>
+                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                    <Image source={{uri: user.image}} style={styles.imageUser} />
+                    <View style={{flexDirection: 'column'}}>
+                        <Text style={{fontSize:20, fontWeight: '600'}}>Hi {user.name}</Text>
+                        <Text>What are you cooking today?</Text>
+                    </View>    
+                </View>
+                <TouchableOpacity>
+                    <Ionicons name="notifications-outline" size={28}/>
+                </TouchableOpacity>
+            </View>
 
-            <Image source={{uri: user.image}} style={styles.image}/>
+            <View style={styles.cookbookContainer}>
+                <View style={styles.cookbookHeader}>
+                    <Text style={[styles.textCookbookHeader, {fontSize: 24}]}>Cookbooks</Text>
+                    <Text style={[styles.textCookbookHeader, {fontSize: 16}]}>1/3</Text>
+                </View>
+                
+                <View style={styles.cookbookSwiper}>                    
+                    <Swiper 
+                        style={styles.swiper} 
+                        showsButtons={false} 
+                        horizontal={true}
+                        autoplay={true}
+                        dotStyle={styles.dot}
+                        activeDotColor='orange'
+                        //onIndexChanged={handleSwiperIndexChange}
+                        //loop={true}
+                    > 
+                        <CookBookComponent{...propsComponent}/>
+                        <CookBookComponent{...propsComponent}/>
+                        <CookBookComponent{...propsComponent}/>           
+                    </Swiper>
+                </View>
+
+
+            </View>
+            {/* <Image source={{uri: user.image}} style={styles.image}/> */}
         </View>
     );
 }
@@ -21,8 +72,54 @@ export default HomeMainView;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        paddingHorizontal: 14,
+        paddingVertical: 16,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        //backgroundColor: 'green'
+    },
+    userContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        height: '15%',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        //marginBottom: 20,
+        // backgroundColor: 'orange',
+    },
+    imageUser: {
+        width: 50,
+        height: 50,
+        borderRadius: 50,
+        marginRight: 15
+    },
+    cookbookContainer: {
+        width: '100%',
+        height: '75%',
+        // backgroundColor: 'orange'
+    },
+    cookbookHeader: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+        //backgroundColor: 'orange'  
+    },
+    textCookbookHeader: {
+        fontWeight: '600'
+    },
+    cookbookSwiper: {
+        width: '100%',
+        height: '100%'
+    },
+    swiper: {
+
+    },
+    dot: {
+        backgroundColor: 'white',
+        borderRadius: 4,
+        borderColor: 'orange',
+        borderWidth: 1
     },
     image: {
         width: 100,
