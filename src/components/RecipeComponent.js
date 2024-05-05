4628ca37926babaf55e0f2c0a1840facc6f63e30
ngochan <20520484@gm.uit.edu.vn>
2024-05-05 12:25:53 +0700

    Update RecipeComponent

--- src/components/RecipeComponent.js
+++ src/components/RecipeComponent.js
@@ -6,7 +6,7 @@ import { useState } from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 function RecipeComponent(props) {
-    const {recipe_name, image, owner, image_owner, likes_amount, reviews_amount} = props;
+    const {recipe_name, image, owner, image_owner, likes_amount, reviews_amount, handleNavDetailRecipe} = props;
 
     const [isFav, setFav] = useState(false);
 
@@ -16,67 +16,72 @@ function RecipeComponent(props) {
 
     return(
         <View style={styles.container}>
-            <Image style={styles.image} source={{uri: image}}/>
-            <Text style={styles.textRecipeName}>{recipe_name}</Text>
+                <Image style={styles.image} source={{uri: image}}/>
 
-            <View style={styles.ownerContainer}>
-                <View style={{flexDirection: 'row', width: '80%'}}>
-                    <Image source={{uri: 'https://i.pinimg.com/736x/3e/02/a5/3e02a58132717af979963f14d5109d80.jpg'}} style={styles.imageOwner}/>
-                    <View style={{width: '40%', justifyContent: 'space-between', }}>
-                        <Text style={{color: 'black', flex: 1}}>{owner}</Text>
-                        
-                        <View style={{flexDirection: 'row', justifyContent: 'space-between', flex: 1}}>
-                            <View style={{flexDirection: 'row', alignItems: 'center',}}>
-                                <Ionicons name="heart" color='orange' size={18}/>
-                                <Text>  {likes_amount}</Text>
-                            </View>
-                        
-                            <Text>.</Text>
-
-                            <View style={{flexDirection: 'row', alignItems: 'center',}}>
-                                <Ionicons name="chatbox" color='orange' size={16}/>
-                                <Text>  {reviews_amount}</Text>
-                            </View>
-                        </View>
-
-                    </View>
+                <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'baseline', paddingHorizontal: 20}}>
+                    <Text style={styles.textRecipeName}>{recipe_name}</Text>
+                    <TouchableOpacity onPress={handleNavDetailRecipe}>
+                        <Text style={{color: 'orange'}}>See detail</Text>
+                    </TouchableOpacity>
                 </View>
-                <TouchableOpacity onPress={toggleFav}>
-                    { isFav === false ? (
-                        <Ionicons name="heart-outline" size={24} color='orange'/>
-                     ) : (
-                        <Ionicons name="heart" size={24} color='orange'/>
-                     )
-                    }
-                </TouchableOpacity>
-            </View>
-            
+
+                <View style={styles.ownerContainer}>
+                    <View style={{flexDirection: 'row', width: '80%'}}>
+                        <Image source={{uri: 'https://i.pinimg.com/736x/3e/02/a5/3e02a58132717af979963f14d5109d80.jpg'}} style={styles.imageOwner}/>
+                        <View style={{width: '40%', justifyContent: 'space-between', }}>
+                            <Text style={{color: 'black', flex: 1}}>{owner}</Text>
+                                
+                            <View style={{flexDirection: 'row', justifyContent: 'space-between', flex: 1}}>
+                                <View style={{flexDirection: 'row', alignItems: 'center',}}>
+                                    <Ionicons name="heart" color='orange' size={18}/>
+                                    <Text>  {likes_amount}</Text>
+                                </View>
+                            
+                                <Text>.</Text>
+
+                                <View style={{flexDirection: 'row', alignItems: 'center',}}>
+                                    <Ionicons name="chatbox" color='orange' size={16}/>
+                                    <Text>  {reviews_amount}</Text>
+                                </View>
+                            </View>
+
+                        </View>
+                    </View>
+                    <TouchableOpacity onPress={toggleFav}>
+                        { isFav === false ? (
+                            <Ionicons name="heart-outline" size={26} color='orange'/>
+                        ) : (
+                            <Ionicons name="heart" size={26} color='orange'/>
+                        )
+                        }
+                    </TouchableOpacity>
+                </View>  
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        height: 350,
+        height: 400,
         backgroundColor: 'white',
         // borderTopLeftRadius: 20,
         // borderTopRightRadius: 20,
         borderRadius: 20,
+        paddingBottom: 16,
         overflow: 'hidden'
 
     },
     image: {
         width: '100%',
-        height: '70%',
+        height: 280,
         // borderRadius: 20,
     },
     textRecipeName: {
         fontSize: 20,
         fontWeight: '600',
         color: 'black',
-        marginTop: 10,
-        marginBottom: 10,
-        marginLeft: 20,
+        marginTop: 16,
+        marginBottom: 16,
     },
     ownerContainer: {
         flexDirection: 'row',
