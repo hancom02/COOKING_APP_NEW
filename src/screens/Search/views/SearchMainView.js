bca3a515d418fb2127e00773246b6a39ca683fa4
hautv140802 <hautv1408@gmail.com>
2024-06-04 20:49:19 +0700

    config svg, font

--- src/screens/Search/views/SearchMainView.js
+++ src/screens/Search/views/SearchMainView.js
@@ -1,24 +1,22 @@
-import { StyleSheet, View, Text } from "react-native";
+import {StyleSheet, View, Text} from 'react-native';
+import HomeIcon from '../../../assets/icons/home.svg';
+const SearchMainView = props => {
+  const {navigation} = props;
 
-const SearchMainView = (props) => {
-    const {
-        navigation, 
-        
-    } = props
-
-    return (
-        <View style={styles.container}>
-            <Text>Search</Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <Text style={{fontFamily: 'Baloo2-Bold'}}>Search</Text>
+      <HomeIcon />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default SearchMainView;
